af7bd26fac62ceb50fe0ea795bb0e31879a5bb87
Jan Heuschele <64351794+DerJH@users.noreply.github.com>
2020-09-09 10:57:56 +0200

    Add files via upload

--- app.R
+++ app.R
@@ -7,23 +7,23 @@ library(magick)
 library(leaflet.extras)
 
 ui <- fluidPage(
-    setBackgroundColor("ghostwhite"),
+    setBackgroundColor("white"),
     h1("Build your own Earth observation poster"),
-    p("Welcome! This website is a compagnion site to the outreach project", tags$strong("We can see our oceans from space - a tailored illustrated guide to promote engagement with Earth Observation"), "funded by the", tags$a(href="https://www.agu.org", "AGU"), "as part of celebrating 100 years of Earth and Space science."),
+    p("Welcome! This website is a companion site to the outreach project", tags$strong("We can see our oceans from space - a tailored illustrated guide to promote engagement with Earth Observation"), "funded by the", tags$a(href="https://www.agu.org", "AGU"), "as part of celebrating 100 years of Earth and Space science."),
     
     p(tags$a(href="https://heuschele.com", "Heuschele.com"),"and the UK", tags$a(href="https://www.noc.ac.uk", "National Oceangraphic Centre"), "worked in partnership to make and share a two-paged poster that explains on one page the basic principles of earth observation and features on the other page a recent satellite image of a specified area."),
     
     p("By running this app you will be able to assemble your own printable two-page pdf version with a satellite image of an area of your choosing. It uses up to date images from the Sentinel satellites of the European Space Agency. This website uses the services provided by", tags$a(href="https://www.sentinel-hub.com", "Sentinel Hub.")),
     
  
-    h2("Step 1"),
+    h2("Step 1: Get access"),
     
    
-    p("Obtain a free trial account from", tags$a(href="https://services.sentinel-hub.com/oauth/subscription", "their website."), "Once you have an account, log in, and go to the Configuration utility to copy the ID given in the WMS template. You will also find the correct ID in user settings under OGC requests. To be able to download the satellite image you need to enter this ID. The script does not save any data, but to be able to make the pdf it will make a temporary copy of the satellite image you download."),
+    p("Obtain a free trial account from", tags$a(href="https://services.sentinel-hub.com/oauth/subscription", "their website."), "Once you have an account, log in, and go to the Configuration utility to copy the ID given in the WMS template (Service endpoint). To be able to download the satellite image you need to enter this ID. The script does not save any data, but to be able to make the pdf it will make a temporary copy of the satellite image you download."),
     
     textInput("caption", "Your Sentinel Hub INSTANCE ID here", "", width = "60%"),
     
-    h2("Step 2"),
+    h2("Step 2: Choose your area"),
     
     p("Drag the map around and zoom into it, and click on the place you are interested in. You can change the place several times."),
     
@@ -31,12 +31,15 @@ ui <- fluidPage(
     
     
     selectInput("band", "Choose the satellite band you want to download",
-                choices = c("Natural", "Vegetation", "MoistureIndex"), width = "60%"),
+                choices = c("Natural", "Vegetation", "MoistureIndex","Agriculture"), width = "60%"),
+    p("(1) Natural will download a natural color image, (2) this image will indicate where vegetation is based on a colorized infrared image, (3) MoistureIndex gives you an indication about the water content of the ground, (4) Agriculture shows agricultural activities"),
     
-    p("(1) Natural will download a natural color image, (2) this image will indicate where vegetation is based on a colorized infrared image, (3) MoistureIndex gives you an indication about the water content of the ground"),
+    h2("Step 3: Choose your language"),
+    selectInput("language", "Choose your poster language",
+                choices = c("English", "French", "German"), width = "60%"),
     
-    h2("Step 3"),
     
+    h2("Step 4: Add a title"),
     p("If you want to add a title onto your satellite image, just change the text below. You can also remove it if you want."),
     textInput("maptext", "", "My favorite place", width = "60%"),
     p("When you are happy with the place, title, and you have entered your ID then press the", tags$strong("go button"), "to generate your personal EO poster. This process might take a while, as the program will download your chosen satellite image and combine it with the explanatory poster, and then assemble a two-sided pdf."),
@@ -47,7 +50,10 @@ ui <- fluidPage(
     downloadLink("downloadData","Click here for your map."),
     br(),
     br(),
-    p("This script/shiny app was written by Jan Heuschele", tags$a(href="http://heuschele.com/","heuschele.com."), "The source code is available on", tags$a(href="https://github.com/DerJH/EOmapmaker","Github"))
+    
+    p("Find some examples and read more about the project on the", tags$a(href="https://noc.ac.uk/education/educational-resources/earth-observation-poster","NOC page.")),
+    
+    p("This shiny app was written by ", tags$a(href="https://heuschele.com/","heuschele.com."), "The source code is available on", tags$a(href="https://github.com/DerJH/EOmapmaker","Github."))
 )
 
 server <- function(input, output, session) {
@@ -78,10 +84,24 @@ server <- function(input, output, session) {
     trara <- reactive({
         switch(input$band,
                "Natural" = "TRUE_COLOR",
-               "Vegetation" = "FALSE_COLOR",
-               "MoistureIndex" = "MOISTURE_INDEX")
+               "Vegetation" = "FALSE-COLOR-VEGETATION",
+               "MoistureIndex" = "MOISTURE_INDEX",
+               "Agriculture" = "AGRICULTURE")
         })
     
+    blabla <- reactive({
+        switch(input$language,
+               "English" = "NOC_english_map.png",
+               "French" = "NOC_french_map.png",
+               "German" = "NOC_german_map.png")
+    })
+    
+    blabla2 <- reactive({
+        switch(input$language,
+               "English" = "NOC_english_frame.png",
+               "French" = "NOC_french_frame.png",
+               "German" = "NOC_german_frame.png")
+    })
     
     observeEvent(input$go, {
         withProgress(message = 'Making plot', value = 0, {
@@ -102,16 +122,15 @@ server <- function(input, output, session) {
                 lower.lon = input$MyMap_click$lng + zoomfac*lichtenbergratio
             }
             
-            
             # Calculate the area 
             coordbox = c( upper.lon,
                           upper.lat,
                           lower.lon,
                           lower.lat)
-            
+
             incProgress(1/n, detail = paste("Calculating the coordinates"))
             incProgress(2/n, detail = paste("Assembling the server request"))
-            
+
             #Website address components
             address = "https://services.sentinel-hub.com/ogc/wms/"
             identifier = input$caption
@@ -122,18 +141,17 @@ server <- function(input, output, session) {
             layers  = "&LAYERS="
             layername = paste(trara())
             today = Sys.Date()
-            lastpart = "&MAXCC10&WIDTH=2500&HEIGHT=1768&FORMAT=image/jpeg&TIME=2019-01-01/"
-        
-            hereyougo = paste(address,identifier,
-                              requeststart,areacoords,
-                              crs,priority,layers,
-                              layername,lastpart,today,
-                              sep = "", collapse = "")
-            
+            lastpart = "&MAXCC10&WIDTH=3300&HEIGHT=2333&FORMAT=image/png&TIME=2020-01-01/"
+             hereyougo = paste(address,identifier,
+                               requeststart,areacoords,
+                               crs,priority,layers,
+                               layername,lastpart,today,
+                               sep = "", collapse = "")
+
             incProgress(5/n, detail = paste("Summoning the servers"))
-            
+
             tryCatch({
-                download.file(hereyougo,'satimage.jpg', mode = 'wb')
+                download.file(hereyougo,'satimage.png', mode = 'wb')
             },
             warning = function(warn){
                 showNotification(paste0(warn), type = 'warning')
@@ -143,14 +161,21 @@ server <- function(input, output, session) {
             })
             
             incProgress(6/n, detail = paste("Downloading the satellite image"))
-            eobs <- image_read("NOC_english_map.jpg")
+            filename = paste(blabla())
+            eobs <- image_read(filename)
+            
+            
             incProgress(7/n, detail = paste("Reading the images"))
-            overlay <- image_read("NOC_english_frame.png")
-            satimage <- image_read("satimage.jpg")
+            
+            filename = paste(blabla2()) 
+            overlay <- image_read(filename)
+          
+            satimage <- image_read("satimage.png")
+            
             incProgress(8/n, detail = paste("Combining the images"))
             
             img <- c(satimage,overlay)
-            img = image_flatten(img)
+            img = image_flatten(img, operator = "Over")
             
             map.t = input$maptext
             img = image_annotate(img, map.t, 
@@ -179,8 +204,6 @@ server <- function(input, output, session) {
             
         }) #end progress bar
     })
-    
-    
 }
 
 
